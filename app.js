b8593a4b4929420abd8d84a30857f6ee5a57d714
badkluster <cabronator_pesuti@hotmail.com>
2021-07-07 18:45:54 -0300

    CREACION USUARIOS
    se crearon los Endopoint a la base de datos para generar los Usuarios

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Configure Header HTTP
-//....
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 //router Basic
 app.use(`/api/${API_VERSION}`, userRoutes);
